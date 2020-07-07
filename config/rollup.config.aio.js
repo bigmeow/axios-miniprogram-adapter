26c4601b83f2e0cd046b1bd7a39da11d65e6ef70
李宗 <lizong@lizongdeMacBook-Pro.local>
2020-07-07 15:00:47 +0800

    upgrade: 脚手架升级

--- config/rollup.config.aio.js
+++ config/rollup.config.aio.js
@@ -1,33 +1,28 @@
 // rollup.config.js
 
-// const uglify = require('rollup-plugin-uglify')
 import common from './rollup'
-import { uglify } from 'rollup-plugin-uglify'
-import nodeResolve from 'rollup-plugin-node-resolve'
-import commonjs from 'rollup-plugin-commonjs'
-
-const isProd = process.env.NODE_ENV === 'production'
+import nodeResolve from '@rollup/plugin-node-resolve'
+import commonjs from '@rollup/plugin-commonjs'
 
 export default {
   input: 'src/index.ts',
   output: {
-    file: isProd ? 'dist/miniprogram/index.js' : 'dist/index.aio.js',
+    file: 'dist/index.aio.js',
     format: 'umd',
     // 如果不同时使用 export 与 export default 可打开legacy
     // legacy: true,
     name: common.name,
     banner: common.banner
   },
-  external: isProd ? [] : common.external,
+  external: common.external,
   plugins: [
     nodeResolve({
       main: true,
-      extensions: [ '.ts', '.js' ]
+      extensions: ['.ts', '.js']
     }),
     commonjs({
       include: 'node_modules/**'
     }),
-    common.getCompiler(),
-    (isProd && uglify())
+    common.getCompiler()
   ]
 }
--- config/rollup.config.js
+++ config/rollup.config.js
@@ -1,11 +1,13 @@
 // rollup.config.js
 
 import common from './rollup'
-import nodeResolve from 'rollup-plugin-node-resolve'
+import nodeResolve from '@rollup/plugin-node-resolve'
+
+const isProd = process.env.NODE_ENV === 'production'
 export default {
   input: 'src/index.ts',
   output: {
-    file: 'dist/index.js',
+    file: isProd ? 'dist/miniprogram/index.js' : 'dist/index.js',
     format: 'cjs',
     // 如果不同时使用 export 与 export default 可打开legacy
     // legacy: true,
@@ -15,7 +17,7 @@ export default {
   plugins: [
     nodeResolve({
       main: true,
-      extensions: [ '.ts', '.js' ]
+      extensions: ['.ts', '.js']
     }),
     common.getCompiler({
       tsconfigOverride: { compilerOptions: { declaration: true } },
